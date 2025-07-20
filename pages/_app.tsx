ffbb025202ef9cf937050d315b61d986970e38e1
alltesting77 <alltesting77@gmail.com>
2025-07-21 00:44:22 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { NavBar } from '../components/NavBar';
 
+// Пример: user сохраняется в state (или из auth hook/context)
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const user = pageProps.user || null; // Заглушка, можно заменить на useAuth()
+  return (
+    <>
+      <NavBar user={user} />
+      <Component {...pageProps} user={user} />
+    </>
+  );
 }
